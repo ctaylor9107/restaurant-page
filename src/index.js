55ef2989a69a31abdc7dc4cadd45d98aff741c9a
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-09-14 09:00:59 -0500

    Create Contact Us section

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './styles.css';
 import {homeDiv} from './home.js';
 import {menuDiv} from './menu.js';
+import {contactDiv} from './contact.js';
 
 const home = homeDiv
 let content = document.querySelector('#content');
 content.appendChild(home);
 
 const homeButton = document.querySelector("#home-button");
-homeButton.addEventListener("click", function clickHomeButton() {
+homeButton.addEventListener('click', function clickHomeButton() {
     const contentChild = content.firstChild;
     contentChild.replaceWith(homeDiv);
 });
@@ -17,3 +18,9 @@ menuButton.addEventListener('click', function clickMenuButton() {
     const contentChild = content.firstChild;
     contentChild.replaceWith(menuDiv);
 })
+
+const contactButton = document.querySelector('#contact-button');
+contactButton.addEventListener('click', function clickMenuButton() {
+    const contentChild = content.firstChild;
+    contentChild.replaceWith(contactDiv);
+});
