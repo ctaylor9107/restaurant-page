0ac0644ebe72d2862710067a30d2c19a727d57b6
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-09-13 19:47:05 -0500

    Add Home and Menu button logic
    Added logic so that when the user clicks the home button or the menu
    button, it will load the appropriate content for that click.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import './styles.css';
 import {homeDiv} from './home.js';
-
-console.log("Hello!!");
+import {menuDiv} from './menu.js';
 
 const home = homeDiv
 let content = document.querySelector('#content');
 content.appendChild(home);
+
+const homeButton = document.querySelector("#home-button");
+homeButton.addEventListener("click", function clickHomeButton() {
+    const contentChild = content.firstChild;
+    contentChild.replaceWith(homeDiv);
+});
+
+const menuButton = document.querySelector('#menu-button');
+menuButton.addEventListener('click', function clickMenuButton() {
+    const contentChild = content.firstChild;
+    contentChild.replaceWith(menuDiv);
+})
---
+++ src/menu.js
@@ -0,0 +1,7 @@
+export const menuDiv = document.createElement('div');
+menuDiv.id = 'menu';
+
+const menuHeader = document.createElement('h1');
+menuHeader.id = 'menu-header';
+menuHeader.textContent = 'Safe Haven Menu';
+menuDiv.appendChild(menuHeader);
